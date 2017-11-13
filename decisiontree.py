a3a512defd2f7bb73df92ab7d7a67fca83373051
jfinocchiaro <22jjf22@gmail.com>
2017-11-12 19:33:11 -0700

    Updated KNN and Decision Tree with pickle

--- decisiontree.py
+++ decisiontree.py
@@ -4,6 +4,7 @@ from sklearn.metrics import accuracy_score
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score
 from sklearn.tree import DecisionTreeClassifier
+import pickle
 
 def scorefunc(pred, test_labels):
     score_count = 0
@@ -20,10 +21,9 @@ def decisiontree(training_data, training_labels, test_data, test_labels):
     prob_predictions = dt.predict_proba(test_data)
     print(prob_predictions)
     predictions = dt.predict(test_data)
-
     accuracy = scorefunc(predictions, test_labels)
-
     print(accuracy)
+
 
 def cross_validate(training_data, training_labels):
     dt = DecisionTreeClassifier()
@@ -32,19 +32,25 @@ def cross_validate(training_data, training_labels):
     print(scores.std() ** 2)
 
 
+
+feat_vectors_file = open('normalized_feature_vec.p','rb')
+feat_vectors = pickle.load(feat_vectors_file)
+feat_vectors_file.close()
+
+tuple_file = open('season_tuples.p','rb')
+feat_labels = pickle.load(tuple_file)
+tuple_file.close()
+
+
+
 X = []
-with open('game-by-game-feature-vectors.csv') as f:
-    for line in f.readlines():
-        line = line.rstrip('\n').split(',')
-        line.pop()
-        line.pop(0)
-        line.pop(0)
-        intline = [float(x) for x in line]
-        X.append(intline)
 y = []
-with open('game-by-game-results.csv') as f:
-    for line in f.readlines():
-        y.append(line.rstrip('\n'))
+for game in feat_labels:
+    X.append(feat_vectors[game[0]][game[1]] + feat_vectors[game[0]][game[2]])
+    y.append(game[3])
+
+print(X[0])
+
 
 
 #y = np.array(y)
--- knn.py
+++ knn.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.metrics import accuracy_score
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split, GridSearchCV
+import pickle
 
 def knn(training_data, training_labels, test_data, test_labels, num_nei):
     nbrs = KNeighborsClassifier(n_neighbors=num_nei, algorithm='ball_tree')
@@ -15,7 +16,7 @@ def knn(training_data, training_labels, test_data, test_labels, num_nei):
     print(accuracy)
 
 def cross_validate(training_data, training_labels, test_data, test_labels):
-    hyperparams= {'n_neighbors' : [1, 2, 3, 5, 7, 9, 11, 15, 19, 21, 25]}
+    hyperparams= {'n_neighbors' : [1, 5, 7, 9, 11, 15 ]}
     nbrs = KNeighborsClassifier()
     gscv = GridSearchCV(nbrs, hyperparams)
     gscv.fit(training_data, training_labels)
@@ -24,21 +25,24 @@ def cross_validate(training_data, training_labels, test_data, test_labels):
     print(gscv.best_estimator_.n_neighbors)
 
 
-X = []
-with open('game-by-game-feature-vectors.csv') as f:
-    for line in f.readlines():
-        line = line.rstrip('\n').split(',')
-        line.pop()
-        intline = [float(x) for x in line]
-        X.append(intline)
-y = []
-with open('game-by-game-results.csv') as f:
-    for line in f.readlines():
-        y.append(line.rstrip('\n'))
+feat_vectors_file = open('normalized_feature_vec.p','rb')
+feat_vectors = pickle.load(feat_vectors_file)
+feat_vectors_file.close()
 
+tuple_file = open('season_tuples.p','rb')
+feat_labels = pickle.load(tuple_file)
+tuple_file.close()
+
+
+
+X = []
+y = []
+for game in feat_labels:
+    X.append(feat_vectors[game[0]][game[1]] + feat_vectors[game[0]][game[2]])
+    y.append(game[3])
 
 print(X[0])
-print(y[0])
+
 
 training_data, test_data, training_labels, test_labels = train_test_split(X, y, test_size=0.2, random_state=42)
 print (len(training_data))
@@ -47,4 +51,5 @@ print (len(test_data))
 print (len(test_labels))
 
 knn(training_data, training_labels, test_data, test_labels, 11)
+
 cross_validate(training_data, training_labels, test_data, test_labels)
