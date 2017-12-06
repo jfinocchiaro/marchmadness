afcdb5f754729228ea00b9434ad35d8ff2540472
Chris Godley <godleyanchor@gmail.com>
2017-12-05 19:10:49 -0700

    ATTENTION!!: To use bracket_buster.py properly, run ./preprocess_data.py first and then ./bracket_buster. Add your models using log_reg as an example. k

--- bracket_buster.py
+++ bracket_buster.py
@@ -17,9 +17,10 @@ class BracketBuster():
 
 	def seed_predict(self, season, team1, team2):
 		print(self.bracket_seeds[season].keys())
-		seed1 = self.bracket_seeds[season][int(team1)]
+		#print("season: %s" % season)
+		seed1 = self.bracket_seeds[season][team1]
 		print(seed1)
-		seed2 = self.bracket_seeds[season][int(team2)]
+		seed2 = self.bracket_seeds[season][team2]
 		return seed1 > seed2
 
 	def log_reg_predict(self, season, team1, team2):
@@ -28,8 +29,12 @@ class BracketBuster():
 		lr = lr.load()
 
 		# Load feature vecs for each team
-		x_vec = list(self.feature_vec[season][team1]) + list(self.feature_vec[season][team2])
-		return lr.predict([x_vec])
+		print("season: %s" % season)
+		try:
+			x_vec = list(self.feature_vec[season][team1]) + list(self.feature_vec[season][team2])
+			return lr.predict([x_vec])
+		except:
+			return -1
 
 	def test_brackets(self):
 		for season in self.bracket_tuples:
@@ -52,8 +57,10 @@ class BracketBuster():
 				if prediction == winner:
 					correct_model_guesses += 1
 
-			accuracy = float(correct_guesses) / total_guesses
-			print("Season: %s\t| Accuracy: %s" % (season, accuracy))
+			accuracy = float(correct_seed_guesses) / total_guesses
+			print("SEED:\tSeason: %s\t| Accuracy: %s" % (season, accuracy))
+			accuracy = float(correct_model_guesses) / total_guesses
+			print("LOGREG:\tSeason: %s\t| Accuracy: %s" % (season, accuracy))
 
 def main():
 	bb = BracketBuster()
