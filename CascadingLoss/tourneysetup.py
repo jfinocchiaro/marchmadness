021aadffb00faab86fefa915a9a911a0244ee5e8
jfinocchiaro <22jjf22@gmail.com>
2017-12-11 15:37:38 -0700

    Parse through ground truth errors.  Still a lot to do

--- CascadingLoss/tourneysetup.py
+++ CascadingLoss/tourneysetup.py
@@ -64,9 +64,10 @@ def calc_ground_truth():
                 ground_truth[season] = {}
 
 
-            for i, game in enumerate(matchups[season]):
+            for game in (bracket_tuples[season]):
+                print(game)
 
-                if (bracket_tuples[season][i][2] == 0):
+                if (bracket_tuples[season][i][2] == 1):
                     winner = bracket_tuples[season][i][0]
                 else:
                     winner = bracket_tuples[season][i][1]
@@ -100,9 +101,10 @@ def perform_predictions():
     seeds = pickle.load(open('../bracket_seeds.p', 'rb'))
     team_list = pickle.load(open('../data/team_list.p', 'rb'))
     feature_vec = pickle.load(open("../AdaBoost/pickled_files/all_feature_vec.p", 'rb'))
+    ground_truth = pickle.load(open('../data/tourney_gt_by_round.p', 'rb'))
 
-    boost = LogReg()
-    boost = pickle.load(open("../log_reg/log_reg.p", 'rb'))
+    lr = LogReg()
+    lr = pickle.load(open("../log_reg/log_reg.p", 'rb'))
 
 
     bracket = {}
@@ -111,48 +113,53 @@ def perform_predictions():
 
 
     for season in matchups:
-        if int(season) >= 2002:
+        if int(season) > 2002:
             print ('Season: %s' % season)
             N_GAMES = len(matchups[season].values())
             i = 0
-            prev_i = 1
             if season not in predictions.keys():
                 predictions[season] = {}
 
-            while(i != prev_i):
-                #if i > 0:
-                    #quit()
-                prev_i = i
+            while(i <= N_GAMES):
+
                 print('i:  %s' % i)
                 for game, teams in matchups[season].items():
                     team1 = teams[0]
                     team2 = teams[1]
                     game_id = game
-                    #print (team1)
+                    #print ('Team 1 %s' %team1)
+                    #print ('Team 2 %s' % team2)
+
+                    #print('Game number %s' % game)
+
                     #print(team1 in team_list)
 
                     #we can actually predict this game
                     if team1 in team_list and team2 in team_list:
-                        pred_winner = random.randint(0,1)
+
                         try:
                             x_vec = list(feature_vec[season][team1]) + list(feature_vec[season][team2])
 
-                            pred_winner = boost.predict([x_vec])[0]
-                            #print(pred_winner)
-
-                            for n_game, n_teams in matchups[season].items():
-                                if game_id in n_teams:
-                                    ind = matchups[season][n_game].index(game_id)
-                                    game_tup = list(matchups[season][n_game])
-                                    game_tup[ind] = teams[pred_winner]
-                                    predictions[season][game_id] = teams[pred_winner]
-                                    matchups[season][n_game] = tuple(game_tup)
-                                    i += 1
-
+                            pred_winner = lr.predict([x_vec])[0]
+                            #print('Pred winner %s' % teams[pred_winner])
+                            #print('Actual winner %s' % ground_truth[season][game_id])
                         except:
-                            print('Skipped a game!')
+                            print('Skipped a game!!')
+                            pred_winner = random.randint(0,1)
 
 
+
+                        for n_game, n_teams in matchups[season].items():
+                            if game_id in n_teams:
+                                ind = matchups[season][n_game].index(game_id)
+                                game_tup = list(matchups[season][n_game])
+                                game_tup[ind] = teams[pred_winner]
+                                predictions[season][game_id] = teams[pred_winner]
+                                matchups[season][n_game] = tuple(game_tup)
+                                i += 1
+
+
+    print ('2009 Champ:  %s' % predictions['2009']['R6CH'])
 
     return matchups, predictions
 
@@ -171,13 +178,12 @@ def calc_acc(predictions):
 
             for game in ground_truth[season].keys():
                 try:
-
                     if predictions[season][game] == ground_truth[season][game]:
                         total_right += 1
                     total_games += 1
                 except:
                     pass
-                quit()
+
             avg_acc.append(float(total_right) / total_games)
 
     print (avg_acc)
@@ -187,7 +193,8 @@ def calc_acc(predictions):
 
 
 if __name__ == '__main__':
+    #write_teams()
     #read_in_tourney_slots()
     #calc_ground_truth()
     matchups, predictions = perform_predictions()
-    calc_acc(predictions)
+    #calc_acc(predictions)
---
+++ preprocess_data_new.py
@@ -0,0 +1,408 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import math
+import re
+import pickle
+import random
+
+from collections import defaultdict
+from matplotlib import pyplot as plt
+from sklearn import preprocessing
+
+def get_decay_time(data, season, team, norm):
+    # Arbitrary normalization
+    return data[season][team]["num_games"] / float(norm)
+
+def init_feature_vector(decay_rate=0, norm=50):
+    dir = "data/"
+    detailed_results = str(dir)+"RegularSeasonDetailedResults.csv"
+
+    data = {}
+    location = {"H": 1, "N": 0, "A": -1}
+    with open(detailed_results, 'rb') as fi:
+        header = fi.readline().decode('utf-8').rstrip('\r\n').split(',')
+
+        #c = 0
+        for lines in fi:
+            '''c += 1
+            if (c == 4616): # 4616 for 2003 data
+                break'''
+
+            # Parse the line
+            l = lines.decode('utf-8').rstrip('\r\n').split(',')
+            #print(len(lineinfo))
+
+            season  = l[0]
+            day_num = l[1]
+            w_team  = l[2]
+            w_score = int(l[3])
+            l_team  = l[4]
+            l_score = int(l[5])
+            w_loc   = l[6]
+            #numot  = l[7]
+
+            # Init for new keys
+            if season not in data:
+                data[season] = {}
+            if w_team not in data[season]:
+                data[season][w_team] = {"total_score": 0, "loc_sum": 0, "num_games": 0, "game_results": [], "end_streak": 0, "max_streak": 0, "total_def_reb": 0, "total_off_reb": 0, "total_num_plays": 0}
+            if l_team not in data[season]:
+                data[season][l_team] = {"total_score": 0, "loc_sum": 0, "num_games": 0, "game_results": [], "end_streak": 0, "max_streak": 0, "total_def_reb": 0, "total_off_reb": 0, "total_num_plays": 0}
+
+            # Standard metrics for winning team
+            data[season][w_team]["num_games"]   += 1
+            decay_w = get_decay_time(data, season, w_team, norm)
+            data[season][w_team]["total_score"] += w_score * decay_w if decay_rate else w_score
+            data[season][w_team]["loc_sum"]     += location[w_loc]
+            data[season][w_team]["game_results"].append(1)
+            data[season][w_team]["end_streak"]  += 1
+            data[season][w_team]["max_streak"]  = max(data[season][w_team]["max_streak"], data[season][w_team]["end_streak"])
+
+            # Standard metrics for losing team
+            data[season][l_team]["num_games"]   += 1
+            decay_l = get_decay_time(data, season, l_team, norm)
+            data[season][l_team]["total_score"] += l_score * decay_l if decay_rate else l_score
+            data[season][l_team]["loc_sum"]     += -location[w_loc]
+            data[season][l_team]["game_results"].append(-1)
+            data[season][l_team]["end_streak"]  = 0
+
+            num_features = 13
+            for i in range(8, 21):
+                if header[i] not in data[season][w_team]:
+                    data[season][w_team][header[i]] = 0
+                if header[i] not in data[season][l_team]:
+                    data[season][l_team][header[i]] = 0
+
+                data[season][w_team][header[i]] += int(l[i]) * decay_w if decay_rate else int(l[i])
+                data[season][l_team][header[i]] += int(l[i+num_features]) * decay_l if decay_rate else int(l[i+num_features])
+
+
+
+            # Winning team additional metrics
+            data[season][w_team]["total_def_reb"] += float(data[season][w_team]["dr"]) / (data[season][w_team]["dr"] + data[season][l_team]["or"]) * decay_w if decay_rate else float(data[season][w_team]["dr"]) / (data[season][w_team]["dr"] + data[season][l_team]["or"])
+            data[season][w_team]["total_off_reb"] += float(data[season][w_team]["or"]) / (data[season][w_team]["or"] + data[season][l_team]["dr"]) * decay_l if decay_rate else float(data[season][w_team]["or"]) / (data[season][w_team]["or"] + data[season][l_team]["dr"])
+
+            #data[season][w_team]["total_num_plays"] = data[season][w_team]["blk"] + data[season][w_team]["stl"] + data[season][w_team]["or"] + data[season][w_team]["dr"]
+
+            # Losing team additional metrics
+            data[season][l_team]["total_def_reb"] += float(data[season][l_team]["dr"]) / (data[season][l_team]["dr"] + data[season][w_team]["or"]) * decay_w if decay_rate else float(data[season][l_team]["dr"]) / (data[season][l_team]["dr"] + data[season][w_team]["or"])
+            data[season][l_team]["total_off_reb"] += float(data[season][l_team]["or"]) / (data[season][l_team]["or"] + data[season][w_team]["dr"]) * decay_l if decay_rate else float(data[season][l_team]["or"]) / (data[season][l_team]["or"] + data[season][w_team]["dr"])
+            #data[season][w_team]["total_num_plays"] = data[season][w_team]["blk"] + data[season][w_team]["stl"] + data[season][w_team]["or"] + data[season][w_team]["dr"]
+
+            #print("Season = %s\t| W_team = %s\t| L_team = %s" % (lineinfo[0], lineinfo[2],lineinfo[4]))
+
+    return data, header
+
+def add_vector_averages(data, header):
+    for season in data:
+        for team in data[season]:
+            num_games = data[season][team]["num_games"]
+
+            for feat in header[8:21]:
+                new_feature = "avg_"+feat
+                total_val = data[season][team][feat]
+
+                data[season][team][new_feature] = float(total_val)/num_games
+
+            # Average Scores
+            total_score = data[season][team]["total_score"]
+            data[season][team]["avg_score"] = total_score / num_games
+
+            # Average rebound percentage
+            data[season][team]["avg_off_reb_percentage"] = data[season][team]["total_off_reb"] / float(num_games)
+            data[season][team]["avg_def_reb_percentage"] = data[season][team]["total_def_reb"] / float(num_games)
+
+    return data
+
+def add_seeds(data):
+    seed_data = "data/TourneySeeds.csv"
+
+    with open(seed_data, "rb") as fi:
+        next(fi)
+        for line in fi:
+            season, seed_string, team = line.decode('utf-8').rstrip('\r\n').split(",")
+            seed = 1 / float(re.findall('\d+', seed_string)[0])
+
+            if int(season) < 2003:
+                continue
+
+            #print("Season: %s\t| seed: %s\t| team: %s" % (season, seed, team))
+
+            data[season][team]["seed_string"] = seed_string
+            data[season][team]["bracket_seed"] = seed
+
+    return data
+
+def add_momentum(data, decay_rate):
+    for season in data:
+        for team in data[season]:
+            data[season][team]["momentum"] = 0
+            num_games = data[season][team]["num_games"]
+
+            for i in range(len(data[season][team]["game_results"])):
+                decay_t = num_games - i
+                game_result = data[season][team]["game_results"][i]
+
+                data[season][team]["momentum"] += game_result * np.exp(-decay_t * decay_rate)
+
+    return data
+
+def add_percentages(data):
+    for season in data:
+        for team in data[season]:
+            data[season][team]["win_percentage"] = 0
+            num_games = data[season][team]["num_games"]
+
+            for i in range(len(data[season][team]["game_results"])):
+                game_result = data[season][team]["game_results"][i]
+
+                if game_result == 1:
+                    data[season][team]["win_percentage"] += 1
+
+            data[season][team]["win_percentage"] /= float(num_games)
+
+            ##
+            data[season][team]["fg_percentage"] = data[season][team]["fgm"] / float(data[season][team]["fga"])
+            data[season][team]["fg3_percentage"] = data[season][team]["fgm3"] / float(data[season][team]["fga3"])
+            data[season][team]["ft_percentage"] = data[season][team]["ftm"] / float(data[season][team]["fta"])
+
+    return data
+
+def decay_test(data):
+    for decay_rate in decay_rates:
+
+        data = add_momentum(data, decay_rate)
+
+        x = []
+        y = []
+        for team in data["2003"]:
+            x.append(team)
+            y.append(data["2003"][team]["momentum"])
+
+        plt.scatter(x, y)
+        plt.title("Decay Rate: %s" % (decay_rate))
+        plt.savefig("decay_rate_"+str(decay_rate)+".png")
+        plt.show()
+
+def print_feature(feat):
+    for season in data:
+        for team in data[season]:
+            print("Season: %s\t| Team: %s\t| %s: %s" % (season, team, feat, data[season][team][feat]))
+
+def load_data():
+    data = pickle.load(open("data.p", "rb"))
+    return data
+
+def dump_data(data):
+    pickle.dump(data, open("data.p", "wb"))
+
+def feature_vectorizor(data, feature_list):
+    feature_vec = {}
+    normalize_vec = []
+
+    for season in data:
+        for team in data[season]:
+            for feature in feature_list:
+                if season not in feature_vec:
+                    feature_vec[season] = {}
+                if team not in feature_vec[season]:
+                    feature_vec[season][team] = []
+
+                if feature not in data[season][team]:
+                    #print("data['%s']['%s']: has no %s" % (season, team, feature))
+                    feature_vec[season][team].append(-1)
+                else:
+                    feature_vec[season][team].append(data[season][team][feature])
+
+            normalize_vec.append(feature_vec[season][team])
+
+    # Normalize
+    features_normalized = preprocessing.normalize(normalize_vec, axis=0)
+
+    #print("Total [season][team]: %s\t| feature_vec: %s" % (len(data)*len(data["2003"]), len(features_normalized)))
+
+    i=0
+    for season in data:
+        for team in data[season]:
+            feature_vec[season][team] = features_normalized[i]
+            i += 1
+
+    return feature_vec
+
+def training_tuples(filename):
+
+    results = {}
+    with open(filename, "rb") as fi:
+        next(fi)
+
+        for lines in fi:
+            # Parse the line
+            l = lines.decode('utf-8').rstrip('\r\n').split(',')
+            #print(len(lineinfo))
+
+            season  = l[0]
+            day_num = l[1]
+            w_team  = l[2]
+            w_score = int(l[3])
+            l_team  = l[4]
+            l_score = int(l[5])
+
+            if random.random() < 0.5:
+                if season not in results:
+                    results[season] = []
+                results[season].append((w_team, l_team, 1))
+            else:
+                if season not in results:
+                    results[season] = []
+                results[season].append((l_team, w_team, 0))
+
+    return results
+
+
+
+def training_tuples_game_id(filename, seedfile):
+
+    results = {}
+    with open(filename, "rb") as fi:
+        next(fi)
+            for lines in fi:
+
+                # Parse the line
+                l = lines.decode('utf-8').rstrip('\r\n').split(',')
+                #print(len(lineinfo))
+
+                season  = l[0]
+                day_num = l[1]
+                w_team  = l[2]
+                w_score = int(l[3])
+                l_team  = l[4]
+                l_score = int(l[5])
+
+                if random.random() < 0.5:
+                    if season not in results:
+                        results[season] = []
+                    results[season].append((w_team, l_team, 1))
+                else:
+                    if season not in results:
+                        results[season] = []
+                    results[season].append((l_team, w_team, 0))
+
+        with open(seedfile, 'rb') as fi2:
+            for line in fi2:
+                l2 = line.decode('utf-8').rstrip('\r\n').split(',')
+                season2 = l2[0]
+                seed = l2[1]
+                team_id = l2[2]
+                if team_id == w_team:
+                    pass
+
+    return results
+
+
+def parse_seeds():
+    tourney_seeds = "data/TourneySeeds.csv"
+
+    bracket_seeds = {}
+    with open(tourney_seeds, 'rb') as fi:
+        next(fi)
+        for lines in fi:
+            l = lines.decode('utf-8').rstrip('\r\n').split(',')
+            #print(l)
+
+            year = l[0]
+            seed = l[1]
+            team = l[2]
+
+            #print("Y: %s\t| S: %s\t| T: %s" % (year, seed, team))
+            if year not in bracket_seeds:
+                bracket_seeds[year] = {}
+            if seed not in bracket_seeds[year]:
+                bracket_seeds[year][seed] = team
+                bracket_seeds[year][team] = int(re.findall(r'\d+', seed)[0])
+
+    return bracket_seeds
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## Initialize variables
+norm = 50
+decay_rate = True
+decay_rates = [1, 1.1, 1.2, 1.3, 1.4, 1.5]
+
+feature_list = ['avg_def_reb_percentage', 'avg_score', 'avg_fgm3', 'avg_dr', 'avg_fga3', 'avg_off_reb_percentage', 'end_streak', 'avg_stl', 'avg_ast', 'fg_percentage', 'avg_or', 'momentum', 'avg_fgm', 'fg3_percentage', 'avg_fga', 'win_percentage', 'num_games', 'avg_blk', 'avg_ftm', 'avg_fta', 'max_streak', 'ft_percentage', 'avg_to', 'avg_pf', 'bracket_seed']
+
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## Initialize data
+#data = load_data()
+#'''
+data_file = "decay_"+str(decay_rate)+"_data.p"
+data = pickle.load(open(data_file, 'rb'))
+#'''
+
+## Generate data
+#data, header = init_feature_vector(decay_rate, norm)
+
+## Add additional metrics
+'''
+data = add_vector_averages(data, header)
+data = add_momentum(data, decay_rates[0])
+data = add_percentages(data)
+data = add_seeds(data)
+'''
+
+## Dump data
+#dump_data(data)
+'''
+data_file = "decay_"+str(decay_rate)+"_data.p"
+pickle.dump(data, open(data_file, "wb"))
+'''
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## Initialize feature_vec
+#'''
+feature_file = "decay_"+str(decay_rate)+"_normalized_feature_vec.p"
+feature_vec = pickle.load(open(feature_file, 'rb'))
+#'''
+
+## Generate Data
+#feature_vec= feature_vectorizor(data, feature_list)
+
+## Dump feature_vec
+#'''
+feature_file = "decay_"+str(decay_rate)+"_normalized_feature_vec.p"
+#pickle.dump(feature_vec, open(feature_file, "wb"))
+#'''
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## Initialize Season data queries
+season_file = "data/RegularSeasonDetailedResults.csv"
+results = training_tuples(season_file)
+results = pickle.load(open("season_tuples.p", 'rb'))
+
+pickle.dump(results, open("season_tuples.p", "wb"))
+#print(results)
+#print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+## Initialize Bracket data queries
+bracket_file = "data/TourneyDetailedResults.csv"
+seed_file = "data/TourneySeeds.csv"
+results = training_tuples(bracket_file, seed_file)
+bracket_seeds = parse_seeds()
+
+#results = pickle.load(open("bracket_tuples.p", 'rb'))
+#bracket_seeds = pickle.load(open("bracket_seeds.p", 'rb'))
+
+pickle.dump(results, open("bracket_tuples.p", "wb"))
+pickle.dump(bracket_seeds, open("bracket_seeds.p", "wb"))
+#print(results)
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## Random printing
+for i in range(len(feature_list)):
+    print("%s:\t%s" % (feature_list[i], feature_vec["2003"]["1104"][i]))
